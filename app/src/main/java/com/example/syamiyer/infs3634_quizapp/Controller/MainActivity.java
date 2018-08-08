6b2b6f66c14eba11c8918efccae79fb90e1a1fd6
Syam Iyer <iyersyam@gmail.com>
2018-08-08 17:01:41 +1000

    Added MVC folders

--- app/src/main/java/com/example/syamiyer/infs3634_quizapp/Controller/MainActivity.java
+++ app/src/main/java/com/example/syamiyer/infs3634_quizapp/Controller/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.syamiyer.infs3634_quizapp.Controllers;
+package com.example.syamiyer.infs3634_quizapp.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
